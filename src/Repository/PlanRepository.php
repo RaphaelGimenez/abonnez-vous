68478beb238f61dacecba8869a2fcc62f0bb2ff5
Raphael Gimenez <contact@raphaelgimenez.fr>
2025-11-25 08:07:06 +0000

    Remove Hello controller and demo code

--- src/Repository/PlanRepository.php
+++ src/Repository/PlanRepository.php
@@ -11,33 +11,8 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class PlanRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Plan::class);
-    }
-
-    //    /**
-    //     * @return Plan[] Returns an array of Plan objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Plan
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, Plan::class);
+  }
 }
--- src/Repository/SubscriptionRepository.php
+++ src/Repository/SubscriptionRepository.php
@@ -11,33 +11,8 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class SubscriptionRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Subscription::class);
-    }
-
-//    /**
-//     * @return Subscription[] Returns an array of Subscription objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Subscription
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, Subscription::class);
+  }
 }
