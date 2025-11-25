f735e0fa73765f1b524aa7a0b7bb3c5f9119e8e5
Raphael Gimenez <contact@raphaelgimenez.fr>
2025-11-25 08:14:42 +0000

    Merge RegistrationController in SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -21,6 +26,50 @@ class SecurityController extends AbstractController
 		return $this->render('security/login.html.twig', [
 			'last_username' => $lastUsername,
 			'error' => $error,
+		]);
+	}
+
+	#[Route('/register', name: 'app_register')]
+	public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+	{
+		// redirect to subscription management if user is already logged in
+		if ($this->getUser()) {
+			/** @var User $user */
+			$user = $this->getUser();
+
+			return $this->redirectToRoute('app_subscription_manage', ['id' => $user->getId()]);
+		}
+
+		$user = new User();
+		$form = $this->createForm(RegistrationFormType::class, $user);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			/** @var string $plainPassword */
+			$plainPassword = $form->get('plainPassword')->getData();
+			/** @var string $email */
+			$email = $form->get('email')->getData();
+
+			$userRepository = $entityManager->getRepository(User::class);
+			$existingUser = $userRepository->findOneByEmail($email);
+
+			// check if user with the same email already exists
+			if ($existingUser) {
+				$this->addFlash('error', 'Une erreur est survenue lors de l\'inscription.');
+				return $this->redirectToRoute('app_register');
+			}
+
+			// hash password
+			$user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+			$entityManager->persist($user);
+			$entityManager->flush();
+
+			return $this->redirectToRoute('app_hello_greet');
+		}
+
+		return $this->render('registration/register.html.twig', [
+			'registrationForm' => $form,
 		]);
 	}
 
