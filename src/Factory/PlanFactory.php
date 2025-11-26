9cd8b3ab9faff3e4b1f7eefd977282683c8c6c1b
Raphael Gimenez <contact@raphaelgimenez.fr>
2025-11-26 13:39:02 +0000

    Stripe lookup keys to PlanFactory

--- src/Factory/PlanFactory.php
+++ src/Factory/PlanFactory.php
@@ -10,41 +10,41 @@ use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
  */
 final class PlanFactory extends PersistentProxyObjectFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
-    public function __construct()
-    {
-    }
+	/**
+	 * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+	 *
+	 * @todo inject services if required
+	 */
+	public function __construct() {}
 
-    public static function class(): string
-    {
-        return Plan::class;
-    }
+	public static function class(): string
+	{
+		return Plan::class;
+	}
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
-     */
-    protected function defaults(): array|callable
-    {
-        return [
-            'monthlyPrice' => self::faker()->randomNumber(),
-            'name' => self::faker()->text(255),
-            'yearlyPrice' => self::faker()->randomNumber(),
-        ];
-    }
+	/**
+	 * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+	 *
+	 * @todo add your default values here
+	 */
+	protected function defaults(): array|callable
+	{
+		return [
+			'monthlyPrice' => self::faker()->randomNumber(),
+			'name' => self::faker()->text(255),
+			'yearlyPrice' => self::faker()->randomNumber(),
+			'stripeMonthlyLookupKey' => self::faker()->text(255),
+			'stripeYearlyLookupKey' => self::faker()->text(255),
+		];
+	}
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
-    protected function initialize(): static
-    {
-        return $this
-            // ->afterInstantiate(function(Plan $plan): void {})
-        ;
-    }
+	/**
+	 * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+	 */
+	protected function initialize(): static
+	{
+		return $this
+			// ->afterInstantiate(function(Plan $plan): void {})
+		;
+	}
 }
